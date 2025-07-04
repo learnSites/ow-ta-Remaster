7c818db595a875851a037ed94123bf9b16b3b8ac
learnSites <learnthingsbetterthenever@gmail.com>
2025-07-04 17:24:15 +0530

    Initail commit

---
+++ owta/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './compontents/footer';
+
+function App() {
+
+  return (
+    <>
+      <div style={{height: '79vh'}}></div>
+      <div style={{background: 'black'}}>
+          <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ owta/src/compontents/footer.jsx
@@ -0,0 +1,51 @@
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <footer className="siteFooter">
+            <div className="footerTop">
+                <div className="footerSection">
+                    <h4>Company</h4>
+                    <div className='inFooterSection'>
+                        <div className='linkWord'>About Us</div>
+                        <div className='spliters'>|</div>
+                        <div className='linkWord'>Careers</div>
+                        <div className='spliters'>|</div>
+                        <div className='linkWord'>Blog</div>
+                    </div>
+                </div>
+                <div className="footerSection">
+                    <h4>Support</h4>
+                    <div className='inFooterSection'>
+                        <div className='linkWord'>FAQs</div>
+                        <div className='spliters'>|</div>
+                        <div className='linkWord'>Contact</div>
+                        <div className='spliters'>|</div>
+                        <div className='linkWord'>Help Center</div>
+                    </div>
+                </div>
+                <div className="footerSection">
+                    <h4>Follow Us</h4>
+                    <div className='inFooterSection'>
+                        <a  className='linkWord' href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+                            <i className="fab fa-facebook-f socialIcon"></i> 
+                        </a>
+                        <a  className='linkWord' href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+                            <i className="fab fa-twitter socialIcon"></i> 
+                        </a>
+                        <a  className='linkWord' href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+                            <i className="fab fa-linkedin-in socialIcon"></i> 
+                        </a>
+                        <a  className='linkWord' href="https://youtube.com" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+                            <i className="fab fa-youtube socialIcon"></i> 
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footerBottom">
+                    &copy; 2025 OW-TA. All rights reserved. <span className='spliters'>| </span> <div style={{textDecoration: 'underline'}} className='linkWord'>Privacy Policy</div>
+            </div>
+        </footer>
+    );
+}
